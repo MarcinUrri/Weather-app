7975d5c42b60e3f7c3db6521c4e87ee5fcfd54e6
Urri <m.sabat@kavastudio.pl>
2019-07-31 00:02:17 +0200

    https release fix

--- src/utils/http-common.js
+++ src/utils/http-common.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-  baseURL: 'http://dev-weather-api.azurewebsites.net/api/city'
+  baseURL: 'https://dev-weather-api.azurewebsites.net/api/city'
 })
